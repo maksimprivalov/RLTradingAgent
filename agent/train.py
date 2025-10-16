12be16543d47b08ed161e96dfde74e3ebc4745ab
maksimprivalov <mprivalovs@gmail.com>
2025-10-16 23:39:14 +0200

    add: Monitor to check the mean episode reward

--- agent/train.py
+++ agent/train.py
@@ -1,12 +1,18 @@
+import os
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 import pandas as pd
 from env import TradingEnv
+from stable_baselines3.common.monitor import Monitor
 
 if __name__ == "__main__":
     df = pd.read_csv("data/MSFT_train.csv")
-
-    env = DummyVecEnv([lambda: TradingEnv(df, window_size=30)])
+    log_dir = "./logs/"
+    os.makedirs(log_dir, exist_ok=True)
+    
+    env = DummyVecEnv([
+    lambda: Monitor(TradingEnv(df, window_size=30), log_dir)
+])
 
     model = PPO(
         "MlpPolicy",
@@ -21,7 +27,7 @@ if __name__ == "__main__":
         tensorboard_log="./logs/"
     )
 
-    model.learn(total_timesteps=400_000)
+    model.learn(total_timesteps=100_000)
 
     model.save("ppo_trader")
 
